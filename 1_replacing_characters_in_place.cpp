b1b940469ae44b5d5454a35fb92af10da6119141
ednux <edsonbrilhante@gmail.com>
2020-06-22 20:55:42 -0400

    Added replace method

--- 1_replacing_characters_in_place.cpp
+++ 1_replacing_characters_in_place.cpp
@@ -1,11 +1,45 @@
 #include <iostream>
+#include <string.h>
 
 using namespace std;
 
+void replace(char *input, int size);
+
 int main(int argc, char **argv)
 {
+    char input[30] = "User is not allowed";
 
-    cout << "Hello World" << endl;
+    replace(input, 19);
+
+    if (strcmp(input, "User&32is&32not&32allowed") == 0)
+    {
+        cout << input << endl;
+    }
+    else
+    {
+        cout << "replace failed" << endl;
+    }
 
     return 0;
+}
+
+void replace(char *input, int size)
+{
+    int i, j;
+
+    for (i=0; i <= size; i++)
+    {
+        if (input[i] == 32)
+        {
+            size+=2;
+            for (j=size; j > (i + 2); j--)
+            {
+                input[j] = input[j-2];
+            }
+
+            input[i] = '&';
+            input[++i] = '3';
+            input[++i] = '2';
+        }
+    }
 }
